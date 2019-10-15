570bdcfd8d44d3c79bea06b04c621dfcd5ddcab6
Laura Levy <llevy510@gmail.com>
2019-10-15 13:31:10 -0400

    initial commit

---
+++ app/src/main/java/com/example/eventguide/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.eventguide;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
