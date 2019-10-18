8a16f3ea0cb1ab4aadc7b76722654026bc6a8c43
Laura Levy <llevy510@gmail.com>
2019-10-18 12:48:12 -0400

    *created eventCardFragment.java file & fragment_event_card.xml & added some basic code to both.

---
+++ app/src/main/java/com/example/eventguide/EventCardFragment.java
@@ -0,0 +1,20 @@
+package com.example.eventguide;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class EventCardFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_event_card, container, false);
+
+        return view;
+    }
+}
