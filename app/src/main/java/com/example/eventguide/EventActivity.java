23a225e28b2424ca2892807607d0281ce79e42c6
Laura Levy <llevy510@gmail.com>
2019-10-20 22:28:41 -0400

    * I added a constraint layout to activity_event.xml * and extended AppCompatActivity in EventActivity.java * theres nothing to send to this activity. yet.

--- app/src/main/java/com/example/eventguide/EventActivity.java
+++ app/src/main/java/com/example/eventguide/EventActivity.java
@@ -1,4 +1,15 @@
 package com.example.eventguide;
 
-public class EventActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+    }
 }
